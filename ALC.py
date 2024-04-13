26bc9fff47569ad5ec0f8dbace3549cd4ed4c992
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-04-12 20:29:20 -0500

    Add files via upload

---
+++ ALC.py
@@ -0,0 +1,7 @@
+'''
+Author: Eliza Malyshev 
+KUID: 3122318
+Date:4/12/2024 
+HackKU Spring 2024 Tutoring Porject; ALC Tutoring webscrapping 
+Purpose: directs and gets information from ALC website 
+''' 
